ce7311d161b7eaec75e433e4ba0ab393bca3b8c0
honza.pofider@seznam.cz <honza.pofider@seznam.cz>
2016-06-12 21:30:47 +0200

    prepare for jsreport 1

---
+++ studio/main_dev.js
@@ -0,0 +1,30 @@
+import DataEditor from './DataEditor.js'
+import Properties from './DataProperties.js'
+import Studio from 'jsreport-studio'
+
+Studio.addEntitySet({
+  name: 'data',
+  faIcon: 'fa-database',
+  visibleName: 'sample data',
+  helpUrl: 'http://jsreport.net/learn/inline-data'
+})
+
+Studio.addPropertiesComponent(Properties.title, Properties, (entity) => entity.__entitySet === 'templates')
+Studio.addEditorComponent('data', DataEditor)
+
+Studio.previewListeners.push((request, entities) => {
+  if (!request.template.data || !request.template.data.shortid) {
+    return
+  }
+
+  // try to fill request.data from the active open tab with sample data
+
+  let dataDetails = Object.keys(entities).map((e) => entities[e])
+    .filter((d) => d.shortid === request.template.data.shortid && d.__entitySet === 'data' && (d.__isLoaded || d.__isNew))
+
+  if (!dataDetails.length) {
+    return
+  }
+
+  request.data = dataDetails[0].dataJson || JSON.stringify({})
+})
