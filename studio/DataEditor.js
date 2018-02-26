6b60267339513739ea97066db6a79c2e2d763a42
BJR Matos <bjrmatos@gmail.com>
2018-02-26 11:03:11 -0500

    fix lint

--- studio/DataEditor.js
+++ studio/DataEditor.js
@@ -5,12 +5,14 @@ export default class DataEditor extends Component {
   render () {
     const { entity, onUpdate } = this.props
 
-    return (<TextEditor
-      name={entity._id}
-      mode='javascript'
-      value={entity.dataJson || ''}
-      onUpdate={(v) => onUpdate(Object.assign({}, entity, {dataJson: v}))}
-      />)
+    return (
+      <TextEditor
+        name={entity._id}
+        mode='javascript'
+        value={entity.dataJson || ''}
+        onUpdate={(v) => onUpdate(Object.assign({}, entity, {dataJson: v}))}
+      />
+    )
   }
 }
 
